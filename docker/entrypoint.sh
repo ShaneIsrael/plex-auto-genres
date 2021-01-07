50f6f35b235f2eb16a7bb53612e0aa76974f16ec
Shane Israel <shanemisrael@gmail.com>
2021-01-07 00:26:23 -0700

    docker config error update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # setup initial config file
 if [ ! -f /config/config.json ]
 then
-    echo "No config.json file found at /config/config.json. Did you volume mount your config?"
+    echo "No config.json file found at /config/config.json. Did you volume mount the config directory?"
 fi
 
 # generate collections once on container start
