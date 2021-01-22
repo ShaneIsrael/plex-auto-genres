237c48c99e92c6cf6be5e5b2371d1a93e7ca2c1c
Shane Israel <shanemisrael@gmail.com>
2021-01-21 22:45:46 -0700

    add log message to let users know that it will continue to run nightly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,5 +10,6 @@ fi
 # generate collections once on container start
 python3 /automate.py
 
+echo "Creating cron process, this script will now run nightly while this container is up."
 # start crond in foreground
 exec crond -f
